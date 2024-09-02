54e1b8df7cc576ea9125c62a9578bd2ad7c3d300
vshtein <v.shtein@uniquerobotics.ru>
2024-09-02 14:20:10 +0300

    Keyboard teleop fix

--- src/bumblebee_teleop/bumblebee_teleop/bumblebee_keyboard_teleop.py
+++ src/bumblebee_teleop/bumblebee_teleop/bumblebee_keyboard_teleop.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 import rclpy
-# from rclpy.qos import qos_profile_default
 from rclpy.qos import QoSProfile, ReliabilityPolicy, HistoryPolicy, DurabilityPolicy
 from geometry_msgs.msg import Twist, TwistStamped
 import sys, select, termios, tty
@@ -11,163 +10,125 @@ msg = """
 Reading from the keyboard  and Publishing to Twist!
 ---------------------------
 Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
+   q    w    e
+   a    s    d
 
-For Holonomic mode (strafing), hold down the shift key:
 ---------------------------
-   U    I    O
-   J    K    L
-   M    <    >
-
-t : up (+z)
-b : down (-z)
 
 anything else : stop
 
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
+Up/Down : increase/decrease max speeds by 10%
+Right/Left : increase/decrease only linear speed by 10%
+z/c : increase/decrease only angular speed by 10%
 
 CTRL-C to quit
 """
 
-# moveBindings = {
-# 		'i':(1,0,0,0),
-# 		'o':(1,0,0,-1),
-# 		'j':(0,0,0,1),
-# 		'l':(0,0,0,-1),
-# 		'u':(1,0,0,1),
-# 		',':(-1,0,0,0),
-# 		'.':(-1,0,0,1),
-# 		'm':(-1,0,0,-1),
-# 		'O':(1,-1,0,0),
-# 		'I':(1,0,0,0),
-# 		'J':(0,1,0,0),
-# 		'L':(0,-1,0,0),
-# 		'U':(1,1,0,0),
-# 		'<':(-1,0,0,0),
-# 		'>':(-1,-1,0,0),
-# 		'M':(-1,1,0,0),
-# 		't':(0,0,1,0),
-# 		'b':(0,0,-1,0),
-# 	       }
-
 moveBindings = {
-		'i':(1,0,0,0),
-		'o':(1,0,0,-1),
-		'j':(0,0,0,1),
-		'l':(0,0,0,-1),
-		'u':(1,0,0,1),
-		',':(-1,0,0,0),
-		'.':(-1,0,0,1),
-		'm':(-1,0,0,-1),
-		'O':(1,-1,0,0),
-		'I':(1,0,0,0),
-		'J':(0,1,0,0),
-		'L':(0,-1,0,0),
-		'U':(1,1,0,0),
-		'<':(-1,0,0,0),
-		'>':(-1,-1,0,0),
-		'M':(-1,1,0,0),
-		't':(0,0,1,0),
-		'b':(0,0,-1,0),
-	       }
+    'w': (1, 0, 0, 0),
+    's': (-1, 0, 0, 0),
+    'a': (0, 0, 0, -1),
+    'd': (0, 0, 0, 1),
+    'e': (1, 0, 0, -1),
+    'q': (1, 0, 0, 1),
+    ' ': (0, 0, 0, 0),
+}
 
-speedBindings={
-		'q':(1.1,1.1),
-		'z':(.9,.9),
-		'w':(1.1,1),
-		'x':(.9,1),
-		'e':(1,1.1),
-		'c':(1,.9),
-	      }
+speedBindings = {
+    '\x1b[A': (1.1, 1.0),  # Вверх
+    '\x1b[B': (0.9, 1.0),  # Вниз
+    '\x1b[D': (1.0, 0.9),  # Влево
+    '\x1b[C': (1.0, 1.1),  # Вправо
+    'z': (1.0, 0.9),
+    'c': (1.0, 1.1),
+}
 
 def getKey():
-	tty.setraw(sys.stdin.fileno())
-	select.select([sys.stdin], [], [], 0)
-	key = sys.stdin.read(1)
-	termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
-	return key
+    tty.setraw(sys.stdin.fileno())
+    rlist, _, _ = select.select([sys.stdin], [], [], 0.1)
+    if rlist:
+        key = sys.stdin.read(1)
+        if key == '\x1b':  # Обработка начала последовательности стрелок
+            key += sys.stdin.read(2)
+    else:
+        key = ''
+    termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    return key
 
-
-def vels(speed,turn):
-	return "currently:\tspeed %s\tturn %s " % (speed,turn)
+def vels(speed, turn):
+    return "currently:\tspeed %s\tturn %s " % (speed, turn)
 
 def main(args=None):
 
-	if args is None:
-		args = sys.argv
+    if args is None:
+        args = sys.argv
 
-	# rclpy.init(args)
-	rclpy.init()
-	node = rclpy.create_node('teleop_twist_keyboard')
+    rclpy.init()
+    node = rclpy.create_node('teleop_twist_keyboard')
 
-	qos_profile = QoSProfile(
-		reliability=ReliabilityPolicy.BEST_EFFORT,
-		durability=DurabilityPolicy.TRANSIENT_LOCAL,
-		history=HistoryPolicy.KEEP_LAST,
-		depth=1
-	)
+    qos_profile = QoSProfile(
+        reliability=ReliabilityPolicy.BEST_EFFORT,
+        durability=DurabilityPolicy.TRANSIENT_LOCAL,
+        history=HistoryPolicy.KEEP_LAST,
+        depth=1
+    )
 
-	# pub = node.create_publisher(Twist, 'cmd_vel', 	qos_profile_default)
-	pub = node.create_publisher(TwistStamped, 'cmd_vel', qos_profile)
+    pub = node.create_publisher(TwistStamped, 'cmd_vel', qos_profile)
 
-	speed = 0.5
-	turn = 1.0
-	x = 0
-	y = 0
-	z = 0
-	th = 0
-	status = 0
+    speed = 0.5
+    turn = 1.0
+    x = 0
+    y = 0
+    z = 0
+    th = 0
+    status = 0
 
-	try:
-		print(msg)
-		print(vels(speed,turn))
-		while(1):
-			key = getKey()
-			if key in moveBindings.keys():
-				x = moveBindings[key][0]
-				y = moveBindings[key][1]
-				z = moveBindings[key][2]
-				th = moveBindings[key][3]
-			elif key in speedBindings.keys():
-				speed = speed * speedBindings[key][0]
-				turn = turn * speedBindings[key][1]
+    try:
+        print(msg)
+        print(vels(speed, turn))
+        while True:
+            key = getKey()
+            if key in moveBindings.keys():
+                x = moveBindings[key][0]
+                y = moveBindings[key][1]
+                z = moveBindings[key][2]
+                th = moveBindings[key][3]
+            elif key in speedBindings.keys():
+                speed = speed * speedBindings[key][0]
+                turn = turn * speedBindings[key][1]
 
-				print(vels(speed,turn))
-				if (status == 14):
-					print(msg)
-				status = (status + 1) % 15
-			else:
-				x = 0
-				y = 0
-				z = 0
-				th = 0
-				if (key == '\x03'):
-					break
+                print(vels(speed, turn))
+                if status == 14:
+                    print(msg)
+                status = (status + 1) % 15
+            else:
+                x = 0
+                y = 0
+                z = 0
+                th = 0
+                if key == '\x03':  # CTRL-C для выхода
+                    break
 
-			twist = TwistStamped()
-			twist.twist.linear.x = x*speed # try
-			twist.twist.linear.y = y*speed
-			twist.twist.linear.z = z*speed
-			twist.twist.angular.x = 0.0
-			twist.twist.angular.y = 0.0
-			twist.twist.angular.z = th*turn
-			pub.publish(twist)
-	except:
-		print(e)
-	finally:
-		twist = TwistStamped()
-		twist.twist.linear.x = 0.0
-		twist.twist.linear.y = 0.0
-		twist.twist.linear.z = 0.0
-		twist.twist.angular.x = 0.0
-		twist.twist.angular.y = 0.0
-		twist.twist.angular.z = 0.0
-		pub.publish(twist)
+            twist = TwistStamped()
+            twist.twist.linear.x = x * speed
+            twist.twist.linear.y = y * speed
+            twist.twist.linear.z = z * speed
+            twist.twist.angular.x = 0.0
+            twist.twist.angular.y = 0.0
+            twist.twist.angular.z = th * turn
+            pub.publish(twist)
 
-		termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
+    except Exception as e:
+        print(e)
 
+    finally:
+        twist = TwistStamped()
+        twist.twist.linear.x = 0.0
+        twist.twist.linear.y = 0.0
+        twist.twist.linear.z = 0.0
+        twist.twist.angular.x = 0.0
+        twist.twist.angular.y = 0.0
+        twist.twist.angular.z = 0.0
+        pub.publish(twist)
 
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
